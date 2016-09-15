6bd902e61118c92fd7b3e0cfc63b2b370a64738a
toshieeeee <macbookairlove@gmail.com>
2016-09-15 18:44:25 +0900

    公開 / 非公開機能実装

--- tool.php
+++ tool.php
@@ -8,7 +8,9 @@ try{
   $data = array();
 
   /***********************************
+
   DB Access INFO
+
   ************************************/
 
   $dsn = 'mysql:dbname=vending_machine;host=localhost';
@@ -17,190 +19,288 @@ try{
   $dbh = new PDO($dsn,$user,$password); //PDO Instance
   $dbh->query('SET NAMES utf8'); // Query run & Access DB
 
+  if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
   /***********************************
-  INSERT 
+
+  ▼INSERT機能 
+
   ************************************/
 
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(isset($_POST['pro_add']) == TRUE){
+
+
+      /***********************************
+      ▼商品名のバリデーション
+      ************************************/
+
+      $pro_name = null;
+
+      if(isset($_POST['pro_name']) !== TRUE || mb_strlen($_POST['pro_name']) === 0){
+
+        $error['pro_name'] = '商品名を入力してください';
+
+      } else if(mb_strlen($_POST['pro_name']) > 20){
+
+        $error['pro_name'] = '商品名は20文字以内で入力してください';
+
+      } else if(preg_match ('/^\s*$|^　*$/',$_POST['pro_name'])){
+
+        $error['pro_name'] = '商品名は半角、または全角スペースだけでは登録できません';
+
+      } else {
+
+        $pro_name = $_POST['pro_name'];    
+
+      }
+
+      /***********************************
+      ▼価格のバリデーション
+      ************************************/
+
+      $pro_price = null;
+
+      if(isset($_POST['pro_price']) !== TRUE || mb_strlen($_POST['pro_price']) === 0){
+
+        $error['pro_price'] = '価格を入力してください';
+
+      } else if(mb_strlen($_POST['pro_price']) > 100){
+
+        $error['pro_price'] = '価格は100文字以内で入力してください';
+
+      } else if(preg_match('/^\s*$|^　*$/',$_POST['pro_price'])){ 
+
+      //[fix]数値のみバリデーションさせる
+
+        $error['pro_price'] = '価格は半角、または全角スペースだけでは登録できません';
+
+      } else if (!preg_match('/[1-9]/',$_POST['pro_price'])){
+
+        $error['pro_price'] = '価格は半角数値のみ入力可能です';
+
+      } else {
+
+        $pro_price = $_POST['pro_price'];
+
+      }
+
+      /***********************************
+      ▼在庫数のバリデーション
+      ************************************/
+
+      $pro_num = null;
+
+      if(isset($_POST['pro_num']) !== TRUE || mb_strlen($_POST['pro_num']) === 0){
+
+        $error['pro_num'] = '在庫数を入力してください';
+
+      } else if(mb_strlen($_POST['pro_num']) > 100){
+
+        $error['pro_num'] = '在庫数は100文字以内で入力してください';
+
+      } else if(preg_match('/^\s*$|^　*$/',$_POST['pro_num'])){ 
+
+      //[must]数値のみバリデーションさせる
+
+        $error['pro_num'] = '在庫数は半角、または全角スペースだけでは登録できません';
+
+
+      } else if(!preg_match('/[1-9]/',$_POST['pro_num'])){
+
+        $error['pro_num'] = '在庫数は半角数値のみ入力可能です';
+
+      } else {
+
+        $pro_num = $_POST['pro_num'];
+
+      }
+
+      /***********************************
+      ▼画像のアップロード
+      ************************************/
+
+      if (is_uploaded_file($_FILES['pro_image']['tmp_name']) === TRUE) {
+
+        $pro_image = $_FILES['pro_image']['name'];  
+
+        $extension = pathinfo($pro_image, PATHINFO_EXTENSION); // 拡張子チェック
+
+        if ($extension === 'jpg' || $extension == 'jpeg' || $extension == 'png') {  
+
+          // ユニークID生成し保存ファイルの名前を変更 
+          //<MEMO> 画像 : 名前を生成するかは、要件によるが、ランダムに生成するパターンが多い
+          //DB : 外部キーは、設定する必要あり 
+
+          $pro_image = md5(uniqid(mt_rand(), true)) . '.' . $extension; 
+
+         // var_dump($pro_image);     
+        
+            // 同名ファイルが存在するか確認 
+
+            if (is_file($img_dir . $pro_image) !== TRUE) { 
+
+              // ファイルを移動し保存
+
+              if (move_uploaded_file($_FILES['pro_image']['tmp_name'], $img_dir . $pro_image) !== TRUE) {
+
+                  $error[] = 'ファイルアップロードに失敗しました';
+
+              } 
+              
+            } else { // 生成したIDがかぶることは通常ないため、IDの再生成ではなく再アップロードを促すようにした 
+
+              $error[] = 'ファイルアップロードに失敗しました。再度お試しください。';
+            }
+
+          } else { 
+
+            $error[] = 'ファイル形式が異なります。画像ファイルはJPEG又はPNGのみ利用可能です。';
+
+          }     
+
+      } else {
+
+        $error[] = 'ファイルを選択してください';
+
+      }
+
+      /***********************************
+      ▼表示ステータスのバリデーション
+      ************************************/
+
+      $pro_status = null;
+
+      if(isset($_POST['pro_status']) !== TRUE){
+
+         $error['pro_status'] = '表示ステータスを選択してください';
+
+      } else {
+
+        $pro_status = $_POST['pro_status'];
+
+      }
+
+      /***********************************
+
+      ▼INSERTを実行
+
+      ************************************/
+
+      if(count($error) === 0){
+
+        $sql = 'INSERT INTO pro_info_table(pro_name,pro_price,pro_image,pro_num,pro_create_date,pro_status) VALUES (?,?,?,?,?,?)';
+        $stmt = $dbh->prepare($sql); 
+        $data[] = $_POST['pro_name']; //[shold] バリでで変数に入れてるので、、
+        $data[] = $_POST['pro_price'];
+        $data[] = $pro_image;
+        $data[] = $_POST['pro_num'];
+        $data[] = date('Y-m-d H:i:s');
+        $data[] = $_POST['pro_status'];
+        
+        
+       // $data[] = $_SERVER['REMOTE_ADDR'];
+        //$data[] = $_SERVER['REMOTE_HOST'];
+
+        $stmt->execute($data);
+        header('Location: http://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); // ブラウザをリダイレクトします
+
+      }
+
+    } //  [$_POST] - pro_add
+
+    if(isset($_POST['pro_update']) == TRUE){
 
     /***********************************
-    ▼Textのバリデーション
+
+    ▼UPDATE機能 
+
     ************************************/
 
-    $pro_name = null;
+      $pro_num = null; 
 
-    if(isset($_POST['pro_name']) !== TRUE || mb_strlen($_POST['pro_name']) === 0){
+      if(isset($_POST['pro_num']) !== TRUE || mb_strlen($_POST['pro_num']) === 0){
 
-      $error['pro_name'] = '商品名を入力してください';
+        $error['pro_num'] = '在庫数を入力してください';
 
-    } else if(mb_strlen($_POST['pro_name']) > 20){
+      } else if(mb_strlen($_POST['pro_num']) > 100){
 
-      $error['pro_name'] = '商品名は20文字以内で入力してください';
+        $error['pro_num'] = '在庫数は100文字以内で入力してください';
 
-    } else if(preg_match ('/^\s*$|^　*$/',$_POST['pro_name'])){
+      } else if(preg_match('/^\s*$|^　*$/',$_POST['pro_num'])){ 
 
-      $error['pro_name'] = '商品名は半角、または全角スペースだけでは登録できません';
+      //[must]数値のみバリデーションさせる
 
-    } else {
-
-      $pro_name = $_POST['pro_name'];    
-
-    }
-
-    //価格のバリデーション
-
-    $pro_price = null;
-
-    if(isset($_POST['pro_price']) !== TRUE || mb_strlen($_POST['pro_price']) === 0){
-
-      $error['pro_price'] = '価格を入力してください';
-
-    } else if(mb_strlen($_POST['pro_price']) > 100){
-
-      $error['pro_price'] = '価格は100文字以内で入力してください';
-
-    } else if(preg_match('/^\s*$|^　*$/',$_POST['pro_price'])){ 
-
-    //[must]数値のみバリデーションさせる
-
-      $error['pro_price'] = '価格は半角、または全角スペースだけでは登録できません';
-
-    } else if (!preg_match('/[1-9]/',$_POST['pro_price'])){
-
-      $error['pro_price'] = '価格は半角数値のみ入力可能です';
-
-    } else {
-
-      $pro_price = $_POST['pro_price'];
-
-    }
-
-    //在庫数のバリデーション
-
-    $pro_num = null;
-
-    if(isset($_POST['pro_num']) !== TRUE || mb_strlen($_POST['pro_num']) === 0){
-
-      $error['pro_num'] = '在庫数を入力してください';
-
-    } else if(mb_strlen($_POST['pro_num']) > 100){
-
-      $error['pro_num'] = '在庫数は100文字以内で入力してください';
-
-    } else if(preg_match('/^\s*$|^　*$/',$_POST['pro_num'])){ 
-
-    //[must]数値のみバリデーションさせる
-
-      $error['pro_num'] = '在庫数は半角、または全角スペースだけでは登録できません';
+        $error['pro_num'] = '在庫数は半角、または全角スペースだけでは登録できません';
 
 
-    } else if(!preg_match('/[1-9]/',$_POST['pro_num'])){
+      } else if(!preg_match('/[1-9]/',$_POST['pro_num'])){
 
-      $error['pro_num'] = '在庫数は半角数値のみ入力可能です';
+        $error['pro_num'] = '在庫数は半角数値のみ入力可能です';
 
-    } else {
+      } else {
 
-      $pro_num = $_POST['pro_num'];
+        $pro_num = $_POST['pro_num']; //一つづつ、UPDATEする際は、POSTする変数を共通にすれば良いのではないだろうか。
+        $pro_id = $_POST['pro_id'];
 
-    }
+      }
 
 
-  /***********************************
-  ▼画像のアップロード
-  ************************************/
 
-    if (is_uploaded_file($_FILES['pro_image']['tmp_name']) === TRUE) {
+      /***********************************
 
-      $pro_image = $_FILES['pro_image']['name'];  
+      ▼UPDATEを実行
 
-      $extension = pathinfo($pro_image, PATHINFO_EXTENSION); // 拡張子チェック
+      ************************************/
 
-      if ($extension === 'jpg' || $extension == 'jpeg' || $extension == 'png') {  
+      if(count($error) === 0){
 
-        // ユニークID生成し保存ファイルの名前を変更            
-        $pro_image = md5(uniqid(mt_rand(), true)) . '.' . $extension; 
+        $sql = 'UPDATE pro_info_table SET pro_num = '.$pro_num.' WHERE pro_id = '.$pro_id;
+        $stmt = $dbh->prepare($sql);         
+        $stmt->execute($data);
 
-       // var_dump($pro_image);     
-      
-          // 同名ファイルが存在するか確認             
-          if (is_file($img_dir . $pro_image) !== TRUE) { 
+        echo "変更しました!";
+       // header('Location: http://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); // ブラウザをリダイレクトします
 
-            // ファイルを移動し保存                 
-            if (move_uploaded_file($_FILES['pro_image']['tmp_name'], $img_dir . $pro_image) !== TRUE) {
+      }
+    
+    } // pro_update
 
-                $error[] = 'ファイルアップロードに失敗しました';
-
-            } 
-            
-          } else { // 生成したIDがかぶることは通常ないため、IDの再生成ではなく再アップロードを促すようにした 
-
-            $error[] = 'ファイルアップロードに失敗しました。再度お試しください。';
-          }
-
-        } else { 
-
-          $error[] = 'ファイル形式が異なります。画像ファイルはJPEG又はPNGのみ利用可能です。';
-
-        }     
-
-    } else {
-
-      $error[] = 'ファイルを選択してください';
-
-    }
-
-    //表示ステータスのバリデーション
-
+    /***********************************
+    ▼ ステータス変更機能
+    ************************************/
 
     $pro_status = null;
 
+    if(isset($_POST['close']) == TRUE){
 
-    if(isset($_POST['pro_status']) !== TRUE){
+        $pro_status = 0;
+        $pro_id = $_POST['pro_id'];
+        $sql = 'UPDATE pro_info_table SET pro_status = '.$pro_status.' WHERE pro_id = '.$pro_id;
+        $stmt = $dbh->prepare($sql);         
+        $stmt->execute($data);
 
-       $error['pro_status'] = '表示ステータスを選択してください';
+    } else if(isset($_POST['open']) == TRUE){
 
-    } else {
-
-      $pro_status = $_POST['pro_status'];
-
-    }
-
-    /***********************************
-    INSERTを実行
-    ************************************/
-
-    if(count($error) === 0){
-
-      $sql = 'INSERT INTO pro_info_table(pro_name,pro_price,pro_image,pro_num,pro_create_date,pro_status) VALUES (?,?,?,?,?,?)';
-      $stmt = $dbh->prepare($sql); 
-      $data[] = $_POST['pro_name']; //[shold] バリでで変数に入れてるので、、
-      $data[] = $_POST['pro_price'];
-      $data[] = $pro_image;
-      $data[] = $_POST['pro_num'];
-      $data[] = date('Y-m-d H:i:s');
-      $data[] = $_POST['pro_status'];
-      
-      
-     // $data[] = $_SERVER['REMOTE_ADDR'];
-      //$data[] = $_SERVER['REMOTE_HOST'];
-
-      $stmt->execute($data); 
-
-      header('Location: http://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); // ブラウザをリダイレクトします
+        $pro_status = 1;
+        $pro_id = $_POST['pro_id'];
+        $sql = 'UPDATE pro_info_table SET pro_status = '.$pro_status.' WHERE pro_id = '.$pro_id;
+        $stmt = $dbh->prepare($sql);         
+        $stmt->execute($data);
 
     }
+
+
 
   } // [$_POST] 
 
 
   /***********************************
+
   SELECT - 一覧情報取得
+
   ************************************/
 
-  $sql = 'SELECT pro_image,pro_name,pro_price,pro_num,pro_status FROM pro_info_table';
-
-  //$sql = 'SELECT pro_name,pro_price,pro_status FROM pro_info_table';
-
+  $sql = 'SELECT pro_id,pro_image,pro_name,pro_price,pro_num,pro_status FROM pro_info_table';
   $stmt = $dbh->prepare($sql);
   $stmt->execute(); 
 
@@ -222,12 +322,48 @@ while(true){
     break;
   }
 
-  $list .= '<tr>';
+  //$list .= '<input type=hidden name="pro_id" value="'.$rec['pro_id'].'">';
+
+  if($rec['pro_status'] === '1'){
+
+    $list .= '<tr class="open">'; // 
+
+  } else{
+
+    $list .= '<tr class="close">';
+
+  }
+
   $list .= '<td>' .$rec['pro_name'].'</td>';
   $list .= '<td>' .$rec['pro_price'].'</td>';
   $list .= '<td><img src="'.$img_dir.$rec['pro_image'].'"</></td>';
-  $list .= '<td class="pro_num">' .$rec['pro_num'].'</td>';
-  $list .= '<td class="pro_status">' .$rec['pro_status'].'</td>';
+
+  $list .= '<form method="post" action="tool.php">
+              <input type=hidden name="pro_id" value="'.$rec['pro_id'].'">
+              <td class="pro_num">
+                <input type="text" name="pro_num"  value=' .$rec['pro_num'].'>
+                <input type="submit" name="pro_update" value="update">
+              </td>
+            </form>';
+
+  $list .= '<form method="post" action="tool.php">
+
+              <input type=hidden name="pro_id" value="'.$rec['pro_id'].'">';
+
+            if($rec['pro_status'] === '1'){
+
+              $list .= '<td>
+                          <input type="submit" name ="close" value="公開 → 非公開">
+                        </td>';
+
+            }else {
+
+              $list .= '<td>
+                          <input type="submit" name ="open" value="非公開 → 公開">
+                        </td>';
+            }
+
+  $list .= '</form>';
   $list .= '</tr>';
 
 }
@@ -267,6 +403,14 @@ PHP Code END
 
   img {
     width: 480px;
+  }
+
+  .open {
+    background: #fff;
+  }
+
+  .close {
+    background: #ccc;
   }
 
   </style>
@@ -317,7 +461,7 @@ PHP Code END
 
       <!--<input type="button" onclick="history.back()" value="戻る">-->
       <br>
-      <input type="submit" value="OK">
+      <input type="submit" name="pro_add" value="OK">
 
     </div>
 
@@ -339,13 +483,9 @@ PHP Code END
           <th>ステータス</th>
       </tr>
 
-      <tr>
-
       <!--ここにPHPのコードを書きます-->
 
         <?php echo $list ?>
-
-      </tr>
 
     </tbody>
 
